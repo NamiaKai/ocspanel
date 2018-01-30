be68d08d1cb29de1f0359c0dcb3caaf73e91fa31
NamiaKai- <admin@namiakai.gq>
2018-01-31 01:06:03 +0800

    Update setting.php

--- application/views/panel/setting.php
+++ application/views/panel/setting.php
@@ -10,21 +10,21 @@
 			<?php if (isset($success)) {echo $success; }?>
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <i class="fa fa-gear fa-fw"></i> Ganti Password
+                    <i class="fa fa-gear fa-fw"></i> Change Password
                 </div>
                 <div class="panel-body">
                     <form role="form" action="<?= base_url('panel/'.$_SESSION['username'].'/setting')?>" method="POST">
 						<input type="hidden" name="username" value="<?= $_SESSION['username'] ?>">
                         <div class="form-group">
-                            <label>Password Lama</label>
+                            <label>Old Password</label>
                             <input class="form-control" placeholder="Old Password" name="oldpass" type="password" required>
                         </div>
                         <div class="form-group">
-                            <label>Password Baru</label>
+                            <label>New Password</label>
                             <input class="form-control" placeholder="New Password" name="password" type="password" required>
                         </div>
                         <div class="form-group">
-                            <label>Masukkan Ulang Password Baru</label>
+                            <label>Re-enter New Password</label>
                             <input class="form-control" placeholder="Re-enter New Password" name="passconf" type="password" required>
                         </div>
                         <button class="btn btn-primary">Save</button>
